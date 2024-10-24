afac947fb13bce5b90efb8c71bd38aef6b2aa7ec
Oky Aneka <okyaneka@gmail.com>
2024-10-24 08:15:43 +0700

    feat(core): add nuxt js core

---
+++ app/modules/public_user/template/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useTitle } from "@vueuse/core";
+
+useTitle("p");
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl text-primary">Welcome to the homepage</h1>
+  </div>
+</template>
